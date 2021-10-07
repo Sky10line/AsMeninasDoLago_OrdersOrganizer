c3fd9fcc0109d23e379832c818b40c404c9311dd
Sky10line <rodrigo_ryo@hotmail.com>
2021-10-07 19:37:45 -0300

    Project Created

---
+++ AsMeninasDoLago_OrdersOrganizer/AsMeninasDoLago_OrdersOrganizer/AsMeninasDoLago_OrdersOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsMeninasDoLago_OrdersOrganizerApp.swift
+//  AsMeninasDoLago_OrdersOrganizer
+//
+//  Created by Rodrigo Ryo Aoki on 07/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AsMeninasDoLago_OrdersOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
