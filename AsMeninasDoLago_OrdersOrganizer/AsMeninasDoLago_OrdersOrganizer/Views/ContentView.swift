dfc4a1aa3f3721bcc57f85172461b6baa12ca4c8
carolinevianab <carolinevb123@gmail.com>
2021-10-17 23:16:46 -0300

    Table view funciona

--- AsMeninasDoLago_OrdersOrganizer/AsMeninasDoLago_OrdersOrganizer/Views/ContentView.swift
+++ AsMeninasDoLago_OrdersOrganizer/AsMeninasDoLago_OrdersOrganizer/Views/ContentView.swift
@@ -10,37 +10,27 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var showModal = false
-    @State private var screenHeight: CGFloat = 700
-    
     
     var body: some View {
-        
-        GeometryReader { geometry in
             ZStack {
-                Color.yellow
-                    .ignoresSafeArea()
                 
-                    Button(action: {
-                            showModal = true
-                        screenHeight = geometry.size.height
-                        
-                    }, label: {
-                        Text("Button")
-                    })
-                    .padding(.vertical, 20)
-                    .padding(.horizontal, 40)
-                    
-                    //modalView(isShowing: $showModal, maxH: $screenHeight)
-                //modalView(isShowing: $showModal, parentHeight: $screenHeight)
+                // Botão auxiliar pra abrir a modal
+                Button(action: {
+                        showModal = true
+                }, label: {
+                    Text("Button")
+                })
+                .padding(.vertical, 20)
+                .padding(.horizontal, 40)
                     
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .sheet(isPresented: $showModal, content: {
-                modalView(isShowing: $showModal, parentHeight: $screenHeight)
-                    .ignoresSafeArea()
-            })
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .sheet(isPresented: $showModal, content: {
+            modalView()
+                .ignoresSafeArea()
+        })
             
-        }
+        
     }
     
     
--- AsMeninasDoLago_OrdersOrganizer/AsMeninasDoLago_OrdersOrganizer/modalView.swift
+++ AsMeninasDoLago_OrdersOrganizer/AsMeninasDoLago_OrdersOrganizer/modalView.swift
@@ -11,78 +11,113 @@ struct modalView: View {
     
     @Environment(\.presentationMode) var presentation
     
+    // Dummy data
+    var data: [testData] = dataa
+    
     var body: some View {
             ZStack (alignment: .bottom) {
                 VStack (alignment: .leading) {
+                    
+                    // Botão de fechar
                     Button(action: { presentation.wrappedValue.dismiss()
                     }, label: {
                         Label("", systemImage: "xmark.circle")
                     })
+                    .padding()
                     
-                    ScrollView(.vertical, showsIndicators: false, content: {
-                        LazyVStack (alignment: .leading) {
-//                            ForEach(1...30, id: \.self){
-//                                Text("Olá \($0)")
-//                            }
-                            tableView
+                    // Table View com a Dummy Data
+                    List {
+                        ForEach(data, id: \.self) { el in
+                            tableCell(item: el)
                         }
-                    })
+                        //.listRowBackground(Color.red)
+                    }
+
                 
             }
-            .padding(10)
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
-            .background(Color.green)
         }
     }
+
+}
+
+struct tableCell: View {
+    var item: testData
     
-    var tableView: some View {
-        ZStack {
-            VStack {
-                ForEach(Range(1...10)){_ in
-                    tableCell
-                }
-                
-            }
-        }
-    }
-    
-    var tableCell: some View {
+    var body: some View {
+        
         HStack {
-            Text("Qtd")
-            
-            Image(systemName: "square")
+            // Quantidade
+            Text(item.qtd).listRowBackground(Color.red)
+
+            // Imagem
+            Image(systemName: item.img)
                 .resizable()
                 .scaledToFit()
                 .frame(height: 100)
             
             VStack (alignment: .leading, spacing: 5) {
-                Text("Nome da comida")
+                // Nome do item
+                Text(item.nome)
                     .fontWeight(.bold)
                     .lineLimit(2)
-                Text ("Preço da comida")
-            }
-            
-            
-        }
-    }
-    
+                
+                // Preço do item
+                Text (item.preco)
 
+            }
+
+        }
+        .padding(.vertical, 5)
+    }
+        
 }
 
 struct modalView_Previews: PreviewProvider {
     static var previews: some View {
-        //modalView(isShowing: .constant(true))
         ContentView()
     }
 }
 
 
-
-struct testData {
+// Struct da Dummy Data
+struct testData: Hashable, Identifiable {
+    var id = UUID()
     var img: String
     var nome: String
     var preco: String
     var qtd: String
 }
 
-let data = [testData(img: "circle", nome: "comida1", preco: "22", qtd: "3"), testData(img: "square", nome: "comida2", preco: "36", qtd: "1")]
+// Dummy Data
+let dataa = [
+    testData(img: "circle", nome: "comida1", preco: "22", qtd: "3"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1"),
+    testData(img: "square", nome: "comida2", preco: "36", qtd: "1")
+]
